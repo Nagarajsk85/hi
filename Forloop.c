c90c16d877891b52f53037274a7629e55b9bfcf7
Nagarajsk85 <69401762+Nagarajsk85@users.noreply.github.com>
2020-08-20 01:49:22 +0530

    Created forloop.c
    This prints a right angle triangle by numbers on the screen

---
+++ Forloop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	//1. loop variable
+	//2. condition
+	int i=01;
+	int j=01;
+	for(i=1;i<=6;i++)
+	{
+		
+		for(j=1;j<=i;j++)
+		{
+			printf("01\t");
+		}
+		printf("\n");
+	}
+}
